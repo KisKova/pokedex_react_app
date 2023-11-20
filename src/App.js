eef85da51e1f86ffb0af5beb958c2e9a6affdbc7
marcellkovacs <kovacsmarci927@gmail.com>
2023-11-20 19:40:20 +0100

    This is the latest update.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 
-import {BrowserRouter as Routes, Route, Router} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Router} from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Pokemon from "./pages/Pokemon";
 
 function App() {
-  return (
-      <Router basename='/pokedex_react_app'>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/pokemon/:id" element={<Pokemon />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/pokemon/:id" element={<Pokemon />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
 
 export default App;
